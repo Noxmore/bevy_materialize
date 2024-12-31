8ed4d8c3685f8a464577120965a136859ca36c9c
noxmore <76889683+noxmore@users.noreply.github.com>
2024-12-31 11:13:49 -0800

    Update full_check.sh

--- full_check.sh
+++ full_check.sh
@@ -3,17 +3,17 @@
 #!/bin/bash
 set -e
 
-echo cargo clippy --all-targets
-cargo clippy --all-targets
+echo cargo clippy
+cargo clippy
 
 echo cargo clippy --no-default-features
 cargo clippy --no-default-features
 
-echo cargo c --features "toml, json" --all-targets
-cargo c --features "toml, json" --all-targets
+echo cargo c --all-features --all-targets
+cargo c --all-features --all-targets
 
 echo cargo fmt --all --check
 cargo fmt --all --check
 
-echo cargo test
-cargo test
+echo cargo test --all-features
+cargo test --all-features
