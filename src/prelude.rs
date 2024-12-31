4793583c238e38e9f59d48341b13f57f6efa521e
noxmore <76889683+noxmore@users.noreply.github.com>
2024-12-31 11:10:45 -0800

    Add MaterialDeserializer in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,4 +2,4 @@
 pub use crate::load::JsonMaterialDeserializer;
 #[cfg(feature = "toml")]
 pub use crate::load::TomlMaterialDeserializer;
-pub use crate::{GenericMaterial, GenericMaterial3d, MaterialProperty, MaterializePlugin};
+pub use crate::{GenericMaterial, GenericMaterial3d, MaterialProperty, MaterializePlugin, load::MaterialDeserializer};
