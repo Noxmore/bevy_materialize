6566185d55445071dc8114e9d5d1491eda202749
noxmore <76889683+noxmore@users.noreply.github.com>
2025-04-07 16:01:02 -0700

    Add `ReflectGenericMaterialLoadAppExt` to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,6 +2,9 @@
 pub use crate::load::JsonMaterialDeserializer;
 #[cfg(feature = "toml")]
 pub use crate::load::TomlMaterialDeserializer;
-pub use crate::{load::MaterialDeserializer, GenericMaterial, GenericMaterial3d, GenericMaterials, MaterialProperty, MaterializePlugin};
+pub use crate::{
+	load::{MaterialDeserializer, ReflectGenericMaterialLoadAppExt},
+	GenericMaterial, GenericMaterial3d, GenericMaterials, MaterialProperty, MaterializePlugin,
+};
 #[cfg(feature = "bevy_pbr")]
 pub use crate::{MaterializeAppExt, ReflectGenericMaterial};
