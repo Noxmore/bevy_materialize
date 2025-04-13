c5cd2ebda2354d4fee6cf1e09dfec5f1e8830389
noxmore <76889683+noxmore@users.noreply.github.com>
2025-04-13 13:23:55 -0700

    Remove extra testing cube

--- examples/example.rs
+++ examples/example.rs
@@ -30,12 +30,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 	));
 
 	commands.spawn((
-		Mesh3d(asset_server.add(Cuboid::from_length(1.).into())),
-		GenericMaterial3d(asset_server.load("materials/example.png")),
-		Transform::from_xyz(0., 1.5, 0.),
-	));
-
-	commands.spawn((
 		Camera3d::default(),
 		Transform::from_translation(Vec3::splat(3.)).looking_at(Vec3::ZERO, Vec3::Y),
 	));
